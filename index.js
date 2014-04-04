d6b885a7ef5b0cf26ad19a009660176ef5e744a0
Maxwell Krohn <themax@gmail.com>
2014-04-04 17:04:49 -0400

    try it out

---
+++ index.js
@@ -0,0 +1,39 @@
+
+var path = require('path');
+
+function sane_os_home() {
+	return process.env.HOME;
+}
+
+function insane_os_home() {
+	var e = process.env.TEMP;
+	if (typeof x !== "undefined" && x !== null) {
+		var p = path.split(e);
+
+		if (p.length === 0) {
+			throw new Error("Malformed temporary path");
+		}
+
+		var t = p[p.length - 1].toLowerCase();
+		if (t === "temp" || t === "tmp") {
+			p.pop()	
+		}
+
+		// The Roaming directory will follow the user around between
+		// computers, which is likely what we want.
+		if (p[p.length - 1].toLowerCase() === "local") {
+			p.push("Roaming");	
+		}
+		return p.join(path.sep);
+	} else {
+		throw new Error("Can't get a temporary directory via env.TEMP");
+	}
+}
+
+module.exports = function() {
+	if (process.platform === "win32") {
+		return insane_os_home();
+	} else {
+		return sane_os_home();
+	}
+}
