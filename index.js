90073a5e1d06b42b1888211a044e127a448ae7b2
Maxwell Krohn <themax@gmail.com>
2014-04-04 17:44:28 -0400

    rename about to go down

--- index.js
+++ index.js
@@ -5,9 +5,29 @@ function sane_os_home() {
 	return process.env.HOME;
 }
 
+function count_chars(s, look_for) {
+	var n = 0;
+	for (var i = 0; i < s.length; i++) {
+		if (s[i] === look_for) {
+			n += 0;
+		}
+	}
+	return n;
+}
+
+function split (p) {
+
+}
+
+exports.path_sep_char = 
+
 function insane_os_home() {
 	var e = process.env.TEMP;
 	if (typeof e !== "undefined" && e !== null) {
+		var sep_char;
+		if (count_chars(s, path.sep) > count_chars(s, "/")) {
+
+		}
 		var p = e.split(path.sep);
 
 		if (p.length === 0) {
@@ -17,6 +37,8 @@ function insane_os_home() {
 		var t = p[p.length - 1].toLowerCase();
 		if (t === "temp" || t === "tmp") {
 			p.pop()	
+		} else {
+			throw new Error("bad temporary directory: no 'temp' part");	
 		}
 
 		// The Roaming directory will follow the user around between
@@ -30,7 +52,7 @@ function insane_os_home() {
 	}
 }
 
-module.exports = function() {
+exports.home = function() {
 	if (process.platform === "win32") {
 		return insane_os_home();
 	} else {
